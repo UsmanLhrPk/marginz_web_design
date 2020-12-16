a9339e69f5a607051874132b1fd7c9865765da52
UsmanLhrPk <usmanjzcn@gmail.com>
2020-12-16 07:52:09 +0500

    fixed nav-link line-height property so it may be contained within nav container

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,26 @@ function closeMenu () {
   console.log(elem.classList);
   elem.classList.remove("open-menu");
 }
+
+function openSearch () {
+  closeMenu();
+  var elem = document.getElementById("search");
+  elem.classList.add("show", "visible");
+  var curElem = document.getElementById("search-button-sm");
+  curElem.classList.remove("display");
+  var logo = document.getElementById("brand-logo");
+  logo.classList.add("hidden"); 
+  var menu = document.getElementById("menu-button");
+  menu.classList.add("hidden");
+}
+
+function closeSearch () {
+  var elem = document.getElementById("search");
+  elem.classList.remove("show", "visible");
+  var curElem = document.getElementById("search-button-sm");
+  curElem.classList.add("display");
+  var logo = document.getElementById("brand-logo");
+  logo.classList.remove("hidden"); 
+  var menu = document.getElementById("menu-button");
+  menu.classList.remove("hidden");
+}  
